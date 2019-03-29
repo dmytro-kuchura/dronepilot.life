14f772dfcb5e136bb65521470e30d80e164db239
Dmitry Kuchura <kuchura.d@gmail.com>
2019-03-29 12:30:52 +0200

    add First Test

---
+++ www/tests/Browser/MainPageTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class MainPageTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @throws \Throwable
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')->assertSee('Laravel');
+        });
+    }
+}
