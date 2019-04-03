c025775f5551aad714b1af71e1413b4485f9f99a
Dmitry Kuchura <kuchura.d@gmail.com>
2019-04-03 09:52:34 +0300

    fix Layouts

--- www/resources/views/work/index.blade.php
+++ www/resources/views/work/index.blade.php
@@ -14,175 +14,44 @@
                 <p>Get all information about our studio from latest news posts & updates page.</p>
             </article>
             <ul class="grid-lod effect-2" id="grid">
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-2.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-4.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-2.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-4.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-2.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
+                @foreach ($left as $item)
+                    @if (isset($item->alias))
+                        <li>
+                            <section class="blog-content">
+                                <a href="{{ route('blog.inner', ['alias' => $item->alias]) }}">
+                                    <figure>
+                                        <div class="post-date">
+                                            {!! date('<\s\p\a\n\>j<\/\s\p\a\n\>F Y', strtotime($item->created_at))  !!}
+                                        </div>
+                                        <img src="images/blog-images/blog-2.jpg" alt="" class="img-responsive"/>
+                                    </figure>
+                                </a>
+                                <article>{{ $item->name }}</article>
+                            </section>
+                        </li>
+                    @endif
+                @endforeach
             </ul>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
             <ul class="grid-lod effect-2" id="grid">
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-1.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-3.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-5.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-1.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-3.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
-                <li>
-                    <section class="blog-content">
-                        <a href="blog-details.html">
-                            <figure>
-                                <div class="post-date">
-                                    <span>24</span> July 2016
-                                </div>
-                                <img src="images/blog-images/blog-5.jpg" alt="" class="img-responsive"/>
-                            </figure>
-                        </a>
-                        <article>
-                            This is a sample news post title content or sample post heading.
-                        </article>
-                    </section>
-                </li>
+                @foreach ($right as $item)
+                    @if (isset($item->alias))
+                        <li>
+                            <section class="blog-content">
+                                <a href="{{ route('blog.inner', ['alias' => $item->alias]) }}">
+                                    <figure>
+                                        <div class="post-date">
+                                            {!! date('<\s\p\a\n\>j<\/\s\p\a\n\>F Y', strtotime($item->created_at))  !!}
+                                        </div>
+                                        <img src="images/blog-images/blog-1.jpg" alt="" class="img-responsive"/>
+                                    </figure>
+                                </a>
+                                <article>{{ $item->name }}</article>
+                            </section>
+                        </li>
+                    @endif
+                @endforeach
             </ul>
         </div>
     </div>
