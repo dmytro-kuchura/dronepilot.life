fdacad23d6d8445e9910af8bb69d9125bbb184c5
Dmitry Kuchura <kuchura.d@gmail.com>
2019-04-03 10:22:05 +0300

    add DB Seeds

--- www/app/Http/Controllers/ProjectsController.php
+++ www/app/Http/Controllers/ProjectsController.php
@@ -2,15 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Repositories\WorkRepository;
+
 class ProjectsController extends Controller
 {
-    public function list()
+    protected $repository;
+
+    public function __construct(WorkRepository $repository)
     {
-        return view('list ');
+        $this->repository = $repository;
     }
 
-    public function inner()
+    public function index()
     {
-        return view('inner');
+        $result = $this->repository->all();
+
+        return view('work.index', [
+            'left' => $result['left'],
+            'right' => $result['right']
+        ]);
+    }
+
+    public function inner(Request $request)
+    {
+        $result = $this->repository->getByAlias($request->route('alias'));
+
+        if (!$result) {
+            abort(404, 'Page not found');
+        }
+
+        return view('work.inner', [
+            'result' => $result
+        ]);
     }
 }
