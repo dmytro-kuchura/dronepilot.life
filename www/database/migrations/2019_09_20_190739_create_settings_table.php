e6fee4d994439be8637f82b42aaa9efb4b9ac67d
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2019-09-20 16:03:12 +0300

    add settings

---
+++ www/database/migrations/2019_09_20_190739_create_settings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMapTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('map', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name', 150);
+            $table->string('key', 150)->unique();
+
+            $table->string('value', 150);
+
+
+            $table->tinyInteger('status')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('map');
+    }
+}
