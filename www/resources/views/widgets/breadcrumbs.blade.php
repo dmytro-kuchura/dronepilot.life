a265a65c0889e849b31dffdea3dc63ab8b3decd9
Dmitry Kuchura <kuchura.d@gmail.com>
2019-11-18 00:01:30 +0200

    add breadcrumbs

--- www/resources/views/widgets/breadcrumbs.blade.php
+++ www/resources/views/widgets/breadcrumbs.blade.php
@@ -4,24 +4,20 @@
             <h1 class="banner-title">{{ $page }}</h1>
             @if(!empty($breadcrumbs))
             <div class="bread-crumb right-side float-none-xs">
-                <ul>
+                <ul itemscope itemtype="http://schema.org/BreadcrumbList">
                     @foreach($breadcrumbs as $key => $bread)
-                        @php
-                            $position = $key + 1;
-                        @endphp
-
                         @if(isset($bread['link']))
                             <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
                                 <a itemprop="item" href="{{ $bread['link'] }}">
                                     <span itemprop="name">{{ $bread['label'] }}</span>
                                 </a>
-                                <meta itemprop="position" content="{{ $position }}" />
+                                <meta itemprop="position" content="{{ $key + 1 }}" />
                             </li>
                             <li><a href="{{ $bread['link'] }}">{{ $bread['label'] }}</a>/</li>
                         @else
                             <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
                                 <span itemprop="name">{{ $bread['label'] }}</span>
-                                <meta itemprop="position" content="{{ $position }}" />
+                                <meta itemprop="position" content="{{ $key + 1 }}" />
                             </li>
                         @endif
                     @endforeach
