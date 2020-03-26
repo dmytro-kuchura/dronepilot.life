264a452dff805f2725b6954d62fd2893d4b9e653
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2020-03-26 17:36:43 +0200

    work Login page

--- www/routes/api.php
+++ www/routes/api.php
@@ -13,29 +13,28 @@
 
 // Fronted Forms
 Route::prefix('v1')->group(function () {
-    Route::post('/comment', 'Api\CommentController@comment')->name('api.comment');
-    Route::post('/contacts', 'Api\ContactsController@contacts')->name('api.contacts');
-    Route::post('/subscribe', 'Api\SubscribeController@subscribe')->name('api.subscribe');
-    Route::post('/image-upload', 'Api\UploadController@upload')->name('api.image');
-
-    Route::prefix('settings')->group(function () {
-        Route::post('/store', 'Admin\SettingsController@store')->name('settings.store');
-        Route::post('/update/{id}', 'Admin\SettingsController@update')->name('settings.update');
+    Route::namespace('Api')->group(function () {
+        Route::post('/comment', 'CommentController@comment')->name('api.comment');
+        Route::post('/contacts', 'ContactsController@contacts')->name('api.contacts');
+        Route::post('/subscribe', 'SubscribeController@subscribe')->name('api.subscribe');
+        Route::post('/image-upload', 'UploadController@upload')->name('api.image');
     });
 });
 
 // Mobile App
 Route::prefix('v2')->group(function () {
     Route::prefix('blog')->group(function () {
-        Route::get('/', 'Api\BlogController@list')->name('blog.list');
-        Route::get('/{id}', 'Api\BlogController@inner')->name('blog.inner');
-        Route::get('/tag/{tag}', 'Api\BlogController@tag')->name('blog.tag');
-        Route::get('/category/{category}', 'Api\BlogController@category')->name('blog.category');
-    });
+        Route::namespace('Api')->group(function () {
+            Route::get('/', 'BlogController@list')->name('blog.list');
+            Route::get('/{id}', 'BlogController@inner')->name('blog.inner');
+            Route::get('/tag/{tag}', 'BlogController@tag')->name('blog.tag');
+            Route::get('/category/{category}', 'BlogController@category')->name('blog.category');
+        });
 
-    Route::prefix('map')->group(function () {
-        Route::get('/', 'Api\MapsController@list')->name('map.list');
-        Route::get('/{id}', 'Api\MapsController@inner')->name('map.inner');
+        Route::prefix('map')->group(function () {
+            Route::get('/', 'MapsController@list')->name('map.list');
+            Route::get('/{id}', 'MapsController@inner')->name('map.inner');
+        });
     });
 });
 
