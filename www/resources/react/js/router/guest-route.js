6c876807b2c8536442cde0f3ccf6393da3d0e05f
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2020-03-26 17:22:29 +0200

    optimize React App

--- www/resources/react/js/router/guest-route.js
+++ www/resources/react/js/router/guest-route.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { useAuth } from '../context/auth';
+import {useAuth} from '../context/auth';
 import useDocumentTitle from '../components/document-title';
 
-function GuestRoute ({ component: Component, title, ...rest }) {
-  useDocumentTitle(title);
+function GuestRoute({component: Component, title, ...rest}) {
+    useDocumentTitle(title);
 
-  let { authenticated } = useAuth();
+    let {authenticated} = useAuth();
 
-  return (
-    <Route
-      {...rest}
-      render={props => authenticated
-        ? <Redirect to={{ pathname: '/home', state: { from: props.location } }} />
-        : <Component {...props} />
-      }
-    />
-  );
-};
+    return (
+        <Route
+            {...rest}
+            render={props => authenticated
+                ? <Redirect to={{pathname: '/home', state: {from: props.location}}}/>
+                : <Component {...props} />
+            }
+        />
+    );
+}
 
 GuestRoute.displayName = 'Guest Route';
 
 GuestRoute.propTypes = {
-  component: PropTypes.func.isRequired,
-  rest: PropTypes.object,
-  location: PropTypes.object,
-  title: PropTypes.string
+    component: PropTypes.func.isRequired,
+    rest: PropTypes.object,
+    location: PropTypes.object,
+    title: PropTypes.string
 };
 
 export default GuestRoute;
--- www/resources/react/js/router/index.js
+++ www/resources/react/js/router/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Welcome from '../pages/welcome';
 import Login from '../pages/auth/login';
 import Register from '../pages/auth/register';
@@ -10,29 +10,29 @@ import Home from '../pages/home';
 import Profile from '../pages/profile';
 import AuthRoute from './auth-route';
 import GuestRoute from './guest-route';
-import { useAuth } from '../context/auth';
+import {useAuth} from '../context/auth';
 import FullPageSpinner from '../components/full-page-spinner';
 
-function App () {
-  let { initializing } = useAuth();
-  return (
-    initializing
-      ? <FullPageSpinner />
-      : <Router>
-        <div className="flex flex-col min-h-screen">
-          <Switch>
-            <GuestRoute exact path="/" component={Welcome} title="welcome" />
-            <GuestRoute path="/register" component={Register} title="register" />
-            <GuestRoute path="/login" component={Login} title="login"/>
-            <GuestRoute path="/forgot-password" component={ForgotPassword} title="forgot password"/>
-            <GuestRoute path="/password/reset/:token" component={ResetPassword} title="reset password"/>
-            <AuthRoute path="/home" component={Home} title="home"/>
-            <AuthRoute path="/profile/:id" component={Profile} title="profile"/>
-            <Route component={NotFound}/>
-          </Switch>
-        </div>
-      </Router>
-  );
+function App() {
+    let {initializing} = useAuth();
+    return (
+        initializing
+            ? <FullPageSpinner/>
+            : <Router>
+                <div className="flex flex-col min-h-screen">
+                    <Switch>
+                        <GuestRoute exact path="/" component={Welcome} title="welcome"/>
+                        <GuestRoute path="/register" component={Register} title="register"/>
+                        <GuestRoute path="/login" component={Login} title="login"/>
+                        <GuestRoute path="/forgot-password" component={ForgotPassword} title="forgot password"/>
+                        <GuestRoute path="/password/reset/:token" component={ResetPassword} title="reset password"/>
+                        <AuthRoute path="/home" component={Home} title="home"/>
+                        <AuthRoute path="/profile/:id" component={Profile} title="profile"/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+            </Router>
+    );
 };
 
 export default App;
