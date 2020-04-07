963a1c9e7d0b4cb12e62e9bd5dca2521af8dd06f
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2020-04-07 18:33:01 +0300

    fix

--- www/app/Http/Controllers/Api/Auth/LoginController.php
+++ www/app/Http/Controllers/Api/Auth/LoginController.php
@@ -2,34 +2,29 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(LoginRequest $request)
     {
-        $this->validate($request, [
-            "email" => "required",
-            "password" => "required",
-        ]);
-
-        $token = auth()->attempt($request->only(["email", "password"]));
+        $token = auth()->attempt($request->all());
 
         if (!$token) {
             return response()->json([
-                "errors" => [
-                    "email" => ["Sorry we couldn\"t sign you in with those details."]
+                'errors' => [
+                    'email' => ['Sorry we couldn\'t sign you in with those details.']
                 ]
             ], 422);
         }
 
         return $this->returnResponse([
-            "user" => new UserResource($request->user()),
-            "token" => Str::random(25),
-            "is_admin" => true
+            'user' => new UserResource($request->user()),
+            'token' => Str::random(25),
+            'is_admin' => true
         ], 200);
     }
 }
